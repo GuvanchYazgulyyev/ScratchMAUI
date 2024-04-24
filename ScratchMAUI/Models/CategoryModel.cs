33a3333e1dfcf300baa10676ade16b008ae1179a
Guvanch Yazgulyyev <77998139+GuvanchYazgulyyev@users.noreply.github.com>
2024-04-25 00:18:44 +0300

    # ScratchMAUI
    ScratchMAUI, .NET MAUI kullanılarak geliştirilen bir uygulamadır. Uygulama, günlük olarak güncellenen ilginç bilgileri görüntülemenizi sağlar. Ana sayfada farklı kategorilerdeki ilginç bilgiler listelenir ve her kategoriye tıklayarak ilgili bilgileri görebilirsiniz. Ayrıca, uygulama rastgele bilgiler de sunar, böylece her gün yeni bir şey öğrenebilirsiniz.
    
    ## Proje Yapısı
    
    - **MainPage**: Ana sayfa, kategorileri listeler ve kullanıcının ilginç bilgilere göz atmasını sağlar.
    - **CategoryFactsPage**: Kategoriye tıklandığında açılan sayfa, seçilen kategoriye ait ilginç bilgileri listeler.
    - **FactDetailPage**: Bir ilginç bilginin ayrıntılarını gösteren sayfa.
    
    ## Kullanılan Teknolojiler
    
    - .NET MAUI
    - Xamarin.Forms
    
    ## Kullanılan Model ve ViewModel Sınıfları
    
    - **CategoryModel**: Kategorilerin adını ve görselini temsil eder.
    - **FactModel**: İlginç bilgilerin metnini ve kaynağını temsil eder.
    - **MainViewModel**: Ana sayfada kullanılacak verileri sağlar.
    
    Bu proje, .NET MAUI'nin temel yapılarını kullanarak bir uygulama geliştirme sürecini göstermektedir.

---
+++ ScratchMAUI/Models/CategoryModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchMAUI.Models
+{
+    public struct CategoryModel
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+        public CategoryModel(string name, string ımage)
+        {
+            Name = name;
+            Image = ımage;
+        }
+
+        public static IEnumerable<CategoryModel> GetCategoryModels()
+        {
+            return new List<CategoryModel>()
+            {
+                new CategoryModel("Animal","lion.png"),
+                new CategoryModel("Human","human.png"),
+                new CategoryModel("Entertainment","entertainment.png"),
+                new CategoryModel("Science & Technology","cience.png"),
+                new CategoryModel("World & Language","world_language.png"),
+                new CategoryModel("Music","music.png"),
+            };
+        }
+    }
+}
---
+++ ScratchMAUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ScratchMAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchMAUI.ViewModel
+{
+   public class MainViewModel
+    {
+        public FactModel FactOfTheDay { get; set; }
+        public IEnumerable<CategoryModel> Categories { get; set; }
+        public IEnumerable<FactModel> RadomFacts { get; set; }
+        public MainViewModel()
+        {
+            Categories = CategoryModel.GetCategoryModels();
+        }
+    }
+}
